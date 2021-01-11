81639900ed165489aac66d2b05c10944ccb4d6c4
zhongzhijie <zhongzhijie@jd.com>
2021-01-11 16:31:17 +0800

    feat(calendar): 自定义日历面板

---
+++ example/components/calendar/index.js
@@ -0,0 +1,30 @@
+Component({
+  properties: {
+    title: String,
+    show: Boolean,
+    dataType: String,
+    dateType: String,
+    value: Array,
+    closeOnClickModal: {
+      type: Boolean,
+      value: true
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    handleClose () {
+
+    },
+    handleDataTypeChange () {
+
+    },
+    handleShortcutChange () {
+
+    },
+    handleCalendarChange () {
+
+    }
+  }
+})
