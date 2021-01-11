186d419f7c5dcd9da1613088e261d863120cb80e
zhongzhijie <zhongzhijie@jd.com>
2021-01-11 22:11:00 +0800

    feat: 京喜拼拼专属日期组件

--- example/components/calendar/index.js
+++ example/components/calendar/index.js
@@ -1,30 +1,235 @@
+/**
+ * 组件用法
+ *   属性                    说明                        类型            可选值             默认值
+ *   show                   是否展示                     boolean         -                false
+ *   dataType               日期类型，’bill‘ 账单日期，    string          ’arrive'         'bill'
+ *                          'arrive' 到账日期
+ *   dateType               快捷类型，'3' 近3天，'7' 近    string          '3', '7', '30'   'custom'
+ *                          7天，'30' 近30天，'custom'
+ *                          自由选择
+ *   value                  dateType为'custom'时自由选    array           -                -
+ *                          择面板的选中值，为2个13位时间戳
+ *   close-on-click-modal   是否点击蒙层可关闭             boolean         -                 true
+ *   formatter              格式化日期展示                function        -                 -
+ *   default-time           日期时间                     array           -                 ['00:00:00', '00:00:00']
+ *   first-day-of-week      周起始天                     number          -                 7
+ *
+ *
+ *  事件                      说明                       参数
+ *  bind:confirm             点击确定按钮触发              event.detail = { show, dataType, dateType, value }
+ *  bind:close               点击蒙层或者关闭按钮时触发     -
+ *  bind:datatypechange      切换 tabs 时触发             event.detail = { dataType }
+ *  bind:datetypechange      切换日期选项时触发            event.detail = { dateType }
+ *  bind:select              点击日期时触发               event.detail = { value }
+ *
+ *  一般只需要用到 bind:confirm 事件即可。
+ *
+ *  <calendaar show="{{ show }}" data-type="{{ dataType }}" date-type="{{ dateType }}" value="{{ value }}" bind:confirm="{{ handleConfirm }}" />
+ *
+ *  Page({
+ *    data: {
+ *      show: false,
+ *      dataType: 'bill',
+ *      dateType: 'custom',
+ *      value: []
+ *    },
+ *    handleConfirm (event) {
+ *      const { show, dataType, dateType, value } = event.detail
+ *      this.setData({
+ *        show,
+ *        dataType,
+ *        dateType,
+ *        value
+ *      })
+ *    }
+ *  })
+ */
 Component({
   properties: {
-    title: String,
-    show: Boolean,
-    dataType: String,
-    dateType: String,
-    value: Array,
+    show: {
+      type: Boolean,
+      observer (val) {
+        if (val) {
+          // 记录原来的值，如果用户没点击确定按钮，则将数据还原
+          const { dataType, dateType, value } = this.data
+          this.setData({
+            lastDataType: dataType,
+            lastDateType: dateType,
+            lastValue: value.slice(0)
+          })
+          // 延迟更新tab的下划线，以及 calendarView 滚动到当前日期或者选中的日期
+          setTimeout(() => {
+            const tabs = this.selectComponent('#tabs')
+            tabs.updateLineStyle(false)
+            const calendarView = this.selectComponent('#calendar-view')
+            calendarView.scrollIntoView()
+          }, 200)
+        }
+      }
+    },
+    dataType: {
+      type: String,
+      value: 'bill'
+    },
+    dateType: {
+      type: String,
+      value: 'custom',
+      observer: 'setBtnDisabled'
+    },
+    value: {
+      type: Array,
+      observer (val) {
+        if (this.data.dateType === 'custom') {
+          this.setData({
+            innerValue: val
+          })
+          this.setBtnDisabled()
+        } else {
+          this.setData({
+            innerValue: []
+          })
+          this.setBtnDisabled()
+        }
+      }
+    },
     closeOnClickModal: {
       type: Boolean,
       value: true
+    },
+    formatter: null,
+    defaultTime: {
+      type: [String, Array],
+      observer (val) {
+        this.setData({
+          formatDefauleTime: this.getDefaultTime(val)
+        })
+      }
+    },
+    firstDayOfWeek: {
+      type: Number,
+      value: 7
     }
   },
   data: {
-
+    innerValue: [],
+    lastDataType: '',
+    lastDateType: '',
+    lastValue: '',
+    btnDisabled: true,
+    formatDefauleTime: []
   },
   methods: {
+    // 设置按钮禁用，只有 dateType 为 custom 且其值不完整时禁用
+    setBtnDisabled () {
+      this.setData({
+        btnDisabled: this.data.dateType === 'custom' && (!this.data.innerValue || !this.data.innerValue.length || !this.data.innerValue[0] || !this.data.innerValue[1])
+      })
+    },
     handleClose () {
-
+      this.setData({
+        show: false
+      })
+      this.triggerEvent('close')
+      setTimeout(() => {
+        // 未点击确定就关闭，还原为原来的值
+        const { lastDataType, lastDateType, lastValue } = this.data
+        this.setData({
+          dataType: lastDataType,
+          dateType: lastDateType,
+          value: lastValue
+        })
+      }, 250)
     },
-    handleDataTypeChange () {
-
+    handleDataTypeChange ({ detail: { name } }) {
+      this.setData({
+        dataType: name
+      })
+      this.triggerEvent('datatypechange', {
+        dataType: name
+      })
     },
-    handleShortcutChange () {
-
+    handleShortcutChange ({ detail: { value } }) {
+      this.setData({
+        dateType: value,
+        innerValue: []
+      })
+      this.triggerEvent('datetypechange', {
+        dateType: value
+      })
+      this.setBtnDisabled()
     },
-    handleCalendarChange () {
+    handleCalendarChange ({ detail: { value } }) {
+      if (this.data.dateType !== 'custom') {
+        this.setData({
+          dateType: 'custom'
+        })
+        this.triggerEvent('datetypechange', {
+          dateType: 'custom'
+        })
+      }
+      this.setData({
+        innerValue: value
+      })
+      this.triggerEvent('select', {
+        value
+      })
+      this.setBtnDisabled()
+    },
+    // 获取完整的 defaultTime
+    getDefaultTime (defaultTime) {
+      if (defaultTime instanceof Array) {
+        const startTime = (defaultTime[0] || '00:00:00').split(':').map(item => {
+          return parseInt(item)
+        })
+        const endTime = (defaultTime[1] || '00:00:00').split(':').map(item => {
+          return parseInt(item)
+        })
+        return [startTime, endTime]
+      } else {
+        const time = (defaultTime || '00:00:00').split(':').map(item => {
+          return parseInt(item)
+        })
 
+        return [time, time]
+      }
+    },
+    // 根据 defaultTime 获取时间
+    getDateByDefaultTime (date, isEnd) {
+      if (!this.data.formatDefauleTime || !this.data.formatDefauleTime.length) return date
+
+      const [startTime, endTime] = this.data.formatDefauleTime || []
+      date = new Date(date)
+      const time = isEnd ? endTime : startTime
+      date.setHours(time[0])
+      date.setMinutes(time[1])
+      date.setSeconds(time[2])
+
+      return date.getTime()
+    },
+    handleConfirm () {
+      this.setData({
+        show: false
+      })
+      const { dataType, dateType } = this.data
+
+      let value = this.data.innerValue
+
+      // 计算近3天、近7天、近30天的开始结束时间
+      if (dateType !== 'custom') {
+        let end = Date.now() - 24 * 60 * 60 * 1000
+        end = this.getDateByDefaultTime(end, true)
+
+        let start = Date.now() - 24 * 60 * 60 * 1000 * parseInt(dateType)
+        start = this.getDateByDefaultTime(start)
+        value = [start, end]
+      }
+
+      this.triggerEvent('confirm', {
+        show: false,
+        dataType: dataType,
+        dateType: dateType,
+        value
+      })
     }
   }
 })
--- example/pages/calendar-pinpin/index.js
+++ example/pages/calendar-pinpin/index.js
@@ -1,5 +1,31 @@
 Page({
   data: {
+    show: false,
+    dataType: 'bill',
+    dateType: 'custom',
+    value: [],
+    formatValue: ''
+  },
+  handleConfirm (event) {
+    const { show, dataType, dateType, value } = event.detail
+    const start = new Date(value[0])
+    const end = new Date(value[1])
 
+    const startStr = `${start.getFullYear()}年${start.getMonth() + 1}月${start.getDate()}日`
+    const endStr = `${end.getFullYear()}年${end.getMonth() + 1}月${end.getDate()}日`
+
+    console.log(start, end)
+    this.setData({
+      show,
+      dataType,
+      dateType,
+      value,
+      formatValue: `${startStr}-${endStr}`
+    })
+  },
+  openCalendar () {
+    this.setData({
+      show: true
+    })
   }
 })
